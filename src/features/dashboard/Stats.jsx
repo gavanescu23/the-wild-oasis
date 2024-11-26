cc9b6719fb2e2425c4d26fe2b2526412687b461a
gavanescu23 <gavanescuvlad@yahoo.com>
2024-11-26 17:44:44 +0200

    Cleaning

--- src/features/dashboard/Stats.jsx
+++ src/features/dashboard/Stats.jsx
@@ -2,7 +2,6 @@ import { HiOutlineBriefcase, HiOutlineChartBar } from "react-icons/hi";
 import Stat from "./Stat";
 import { HiOutlineBanknotes, HiOutlineCalendarDays } from "react-icons/hi2";
 import { formatCurrency } from "../../utils/helpers";
-import Spinner from "../../ui/Spinner";
 
 function Stats({ bookings, confirmedStays, numDays, cabinCount }) {
   const numBookings = bookings?.length;
